39a691e60a5aba8a3d312ccba17bd1806fea5944
Tomasz Klara <tomaszklara@gmail.com>
2022-05-05 22:47:27 +0100

    updates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	addr := flag.String("addr", ":8000", "HTTP Netword Address")
+
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.LstdFlags)
+
+	errorLog := log.New(os.Stderr, "Error\t", log.LstdFlags)
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("dist"))
@@ -18,9 +29,14 @@ func main() {
 	mux.HandleFunc("/admin/record/create", createRecordPost)
 	mux.HandleFunc("/admin/record/new", createRecordGet)
 
-	err := http.ListenAndServe(":8000", mux)
-	if err != nil {
-		log.Fatalln(err)
+	srv := http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
 	}
+
+	infoLog.Printf("Server starting on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 
 }
